114ce877a9999ef342d26c26335f7c2ee543d7ee
Piyush Kumar <piyush.kumar@hmcts.net>
2019-10-08 14:22:52 +0100

    firewall naming

--- modules/azure-firewall/firewall.tf
+++ modules/azure-firewall/firewall.tf
@@ -1,6 +1,6 @@
 resource "azurerm_firewall" "main" {
   count               = length(var.firewall)
-  name                = "${var.project}-${var.env}-${lookup(element(var.firewall, count.index), "location")}"
+  name                = "${var.env}-${lookup(element(var.firewall, count.index), "location")}-fw"
   location            = azurerm_resource_group.main[count.index].location
   resource_group_name = azurerm_resource_group.main[count.index].name
 

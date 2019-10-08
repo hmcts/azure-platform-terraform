23be74a1f6a9069897569e3c2fcd0eef345152d3
Piyush Kumar <piyush.kumar@hmcts.net>
2019-10-08 16:36:16 +0100

    fix the variable

--- modules/azure-firewall/variables.tf
+++ modules/azure-firewall/variables.tf
@@ -1,16 +1,5 @@
 variable "env" {}
 variable "project" {}
-variable "common_tags" {
-    default = {
-    "managedBy"          = "Platform Engineering"
-    "solutionOwner"      = "CFT"
-    "activityName"       = "AKS"
-    "dataClassification" = "Internal"
-    "automation"         = ""
-    "costCentre"         = "10245117" // until we get a better one, this is the generic cft contingency one
-    "environment"        = "sbox"
-    "criticality"        = "Medium"
-  }
-}
+variable "common_tags" {}
 variable "aks" {}
 variable "firewall" {}

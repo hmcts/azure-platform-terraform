21572a996437dec8dd8a540b531f05ce650ccb2e
Tim Jacomb <tim.jacomb@hmcts.net>
2019-10-21 14:26:30 +0100

    Sanitise waf-profile-name

--- modules/azure-landing-zone/frontdoor_waf_policy.tf
+++ modules/azure-landing-zone/frontdoor_waf_policy.tf
@@ -1,6 +1,6 @@
 resource "azurerm_frontdoor_firewall_policy" "custom" {
   count               = length(var.frontends)
-  name                = lookup(element(var.frontends, count.index), "name")
+  name                = replace(lookup(var.frontends[count.index], "name"), "-", "")
   resource_group_name = var.resource_group
   enabled             = true
   mode                = var.waf_mode

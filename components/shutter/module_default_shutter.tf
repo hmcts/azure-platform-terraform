46ba6eb9aa484444e2676d8cc40e2f40244b89c0
Piyush Kumar <piyush.kumar@hmcts.net>
2019-12-09 11:43:25 +0000

    fmt

--- components/shutter/module_default_shutter.tf
+++ components/shutter/module_default_shutter.tf
@@ -1,9 +1,9 @@
 module "plum_shutter" {
-    source               = "../../modules/shutter_page"
-    resource_group_name  = var.resource_group_name
-    env                  = var.env
-    shutter_storage      = var.shutter_storage
-    shutter_apps         = var.shutter_apps
-    cdn_sku              = var.cdn_sku
-    common_tags          = local.common_tags
+  source              = "../../modules/shutter_page"
+  resource_group_name = var.resource_group_name
+  env                 = var.env
+  shutter_storage     = var.shutter_storage
+  shutter_apps        = var.shutter_apps
+  cdn_sku             = var.cdn_sku
+  common_tags         = local.common_tags
 }
--- modules/shutter_page/custom_domains.tf
+++ modules/shutter_page/custom_domains.tf
@@ -3,15 +3,15 @@ data "template_file" "customdomain" {
 }
 
 resource "azurerm_template_deployment" "custom_domain" {
-  count               = length(var.shutter_apps) 
+  count               = length(var.shutter_apps)
   template_body       = data.template_file.customdomain.rendered
   name                = split(".", var.shutter_apps[count.index])[0]
   resource_group_name = data.azurerm_resource_group.shutter.name
   deployment_mode     = "Incremental"
 
   parameters = {
-    name       = "${azurerm_cdn_profile.main.name}/${azurerm_cdn_endpoint.shutter_endpoint[count.index].name}/${replace(var.shutter_apps[count.index], ".", "-")}"
-    hostName   = var.shutter_apps[count.index]
+    name     = "${azurerm_cdn_profile.main.name}/${azurerm_cdn_endpoint.shutter_endpoint[count.index].name}/${replace(var.shutter_apps[count.index], ".", "-")}"
+    hostName = var.shutter_apps[count.index]
   }
 
   depends_on = [azurerm_cdn_endpoint.shutter_endpoint]

e2a61e630ed00c640723849d08d394da3c196ea5
Tim Jacomb <tim.jacomb@hmcts.net>
2019-12-16 14:27:41 +0000

    Strip www

--- modules/shutter_page/custom_domains.tf
+++ modules/shutter_page/custom_domains.tf
@@ -1,11 +1,11 @@
 data "template_file" "customdomain" {
-  template = "${file("${path.module}/templates/custom_domain.json")}"
+  template = file("${path.module}/templates/custom_domain.json")
 }
 
 resource "azurerm_template_deployment" "custom_domain" {
   count               = length(var.shutter_apps)
   template_body       = data.template_file.customdomain.rendered
-  name                = split(".", var.shutter_apps[count.index])[0]
+  name                = split(".", replace(var.shutter_apps[count.index], "www.", ""))[0]
   resource_group_name = data.azurerm_resource_group.shutter.name
   deployment_mode     = "Incremental"
 
@@ -15,4 +15,4 @@ resource "azurerm_template_deployment" "custom_domain" {
   }
 
   depends_on = [azurerm_cdn_endpoint.shutter_endpoint]
-} 
+}

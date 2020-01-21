6d27313945a773d9991a127abca67da3d36dcece
Tim Jacomb <tim.jacomb@hmcts.net>
2020-01-21 08:03:50 +0000

    Rebuild prod CDN

--- components/shutter/module_default_shutter.tf
+++
@@ -1,9 +0,0 @@
-module "default_shutter" {
-  source              = "../../modules/shutter_page"
-  resource_group_name = var.resource_group_name
-  env                 = var.env
-  shutter_storage     = var.shutter_storage
-  shutter_apps        = var.shutter_apps
-  cdn_sku             = var.cdn_sku
-  common_tags         = local.common_tags
-}

0901efb189dbd01fba7d6e01ed3735d48ef327c0
Tim Jacomb <tim.jacomb@hmcts.net>
2020-01-23 14:09:32 +0000

    Fix fmt

--- components/shutter/module_default_shutter.tf
+++ components/shutter/module_default_shutter.tf
@@ -1,10 +1,10 @@
 module "default_shutter" {
-  source              = "../../modules/shutter_page"
-  resource_group_name = var.resource_group_name
-  env                 = var.env
-  shutter_storage     = var.shutter_storage
-  frontends           = var.frontends
-  cdn_sku             = var.cdn_sku
-  common_tags         = local.common_tags
+  source                     = "../../modules/shutter_page"
+  resource_group_name        = var.resource_group_name
+  env                        = var.env
+  shutter_storage            = var.shutter_storage
+  frontends                  = var.frontends
+  cdn_sku                    = var.cdn_sku
+  common_tags                = local.common_tags
   certificate_key_vault_name = var.certificate_key_vault_name
 }
--- modules/shutter_page/enable_https.tf
+++ modules/shutter_page/enable_https.tf
@@ -8,10 +8,10 @@ resource "null_resource" "enable_custom_https_cmd" {
 
 json='
 ${templatefile("${path.module}/templates/customHttps.json", {
-    secret_name: var.frontends[count.index].certificate_name,
-    resource_group: data.azurerm_resource_group.shutter.name,
-    subscription_id: data.azurerm_client_config.current.subscription_id,
-    vault_name: var.certificate_key_vault_name
+    secret_name : var.frontends[count.index].certificate_name,
+    resource_group : data.azurerm_resource_group.shutter.name,
+    subscription_id : data.azurerm_client_config.current.subscription_id,
+    vault_name : var.certificate_key_vault_name
 })}'
 
 
@@ -19,7 +19,7 @@ az rest --method POST \
  --uri "https://management.azure.com/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.shutter.name}/providers/Microsoft.Cdn/profiles/${azurerm_cdn_profile.main.name}/endpoints/${azurerm_cdn_endpoint.shutter_endpoint[count.index].name}/customDomains/${replace(var.frontends[count.index].custom_domain, ".", "-")}/enableCustomHttps?api-version=2019-04-15" \
  --body "$json" || true # this can only be run if it's not been run before or its in the completed state.
 EOF
-  }
+}
 
-  depends_on = [azurerm_template_deployment.custom_domain]
+depends_on = [azurerm_template_deployment.custom_domain]
 }

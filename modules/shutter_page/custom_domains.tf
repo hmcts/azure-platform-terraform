76155a2e2421ac270af537dcb54d034791ad9bc5
Tim Jacomb <tim.jacomb@hmcts.net>
2020-01-23 14:27:01 +0000

    Try fix shuttering (#72)

--- modules/shutter_page/custom_domains.tf
+++ modules/shutter_page/custom_domains.tf
@@ -3,15 +3,15 @@ data "template_file" "customdomain" {
 }
 
 resource "azurerm_template_deployment" "custom_domain" {
-  count               = length(var.shutter_apps)
+  count               = length(var.frontends)
   template_body       = data.template_file.customdomain.rendered
-  name                = split(".", replace(var.shutter_apps[count.index], "www.", ""))[0]
+  name                = split(".", replace(var.frontends[count.index].custom_domain, "www.", ""))[0]
   resource_group_name = data.azurerm_resource_group.shutter.name
   deployment_mode     = "Incremental"
 
   parameters = {
-    name     = "${azurerm_cdn_profile.main.name}/${azurerm_cdn_endpoint.shutter_endpoint[count.index].name}/${replace(var.shutter_apps[count.index], ".", "-")}"
-    hostName = var.shutter_apps[count.index]
+    name     = "${azurerm_cdn_profile.main.name}/${azurerm_cdn_endpoint.shutter_endpoint[count.index].name}/${replace(var.frontends[count.index].custom_domain, ".", "-")}"
+    hostName = var.frontends[count.index].custom_domain
   }
 
   depends_on = [azurerm_cdn_endpoint.shutter_endpoint]
--- modules/shutter_page/variables.tf
+++ modules/shutter_page/variables.tf
@@ -2,5 +2,10 @@ variable "resource_group_name" {}
 variable "env" {}
 variable "common_tags" {}
 variable "shutter_storage" {}
-variable "shutter_apps" {}
+variable "frontends" {
+  type = list(object({
+    name = string, custom_domain = string, certificate_name = string
+  }))
+}
 variable "cdn_sku" {}
+variable "certificate_key_vault_name" {}

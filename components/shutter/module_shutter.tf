c7b1aeaabafa99df938b0d3225f569f2df0194ae
Piyush Kumar <piyush.m82@gmail.com>
2020-02-04 14:10:15 +0000

    Shutter improvement (#93)
    * improve shutter module
    
    * default continue
    
    * update
    
    * storage for everything

--- components/shutter/module_shutter.tf
+++ components/shutter/module_shutter.tf
@@ -1,9 +1,8 @@
-module "default_shutter" {
+module "shutter" {
   source                     = "../../modules/shutter_page"
   resource_group_name        = var.shutter_rg
   env                        = var.env
-  shutter_storage            = var.shutter_storage
-  shutter_apps               = var.default_shutter
+  shutter_apps               = var.frontends
   common_tags                = local.common_tags
   certificate_key_vault_name = var.certificate_key_vault_name
   subscription               = var.subscription
--- modules/shutter_page/storage_container.tf
+++ modules/shutter_page/storage_container.tf
@@ -1,15 +1,17 @@
 resource "azurerm_storage_container" "containers" {
+  count                 = length(var.shutter_apps)
   name                  = "$web"
-  storage_account_name  = azurerm_storage_account.shutter_store.name
+  storage_account_name  = azurerm_storage_account.shutter_store[count.index].name
   container_access_type = "private"
 }
 
 resource "null_resource" "static_website_cmd" {
+  count = length(var.shutter_apps)
   provisioner "local-exec" {
     command = <<EOF
 az storage blob service-properties update \
   --subscription "${data.azurerm_subscription.current.subscription_id}" \
-  --account-name "${azurerm_storage_account.shutter_store.name}" \
+  --account-name "${azurerm_storage_account.shutter_store[count.index].name}" \
   --static-website \
   --404-document index.html \
   --index-document index.html

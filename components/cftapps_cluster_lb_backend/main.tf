2108a9a2961196b4c694eb8032e08713e61f7b10
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2020-03-11 11:49:41 +0000

    added artifacts for sbox backend app gw component

--- components/cftapps_cluster_lb_backend/main.tf
+++ components/cftapps_cluster_lb_backend/main.tf
@@ -1,6 +1,6 @@
 module "app-gw" {
   source    = "../../modules/app-gateway-module-backend"
-  yaml_path = "${path.cwd}/../environments/${var.env}/all.yaml"
+  yaml_path = "${path.cwd}/../../environments/${var.env}/all.yaml"
   env       = var.env
 
   location           = var.location

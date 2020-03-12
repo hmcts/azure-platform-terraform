12c9215326c6e457e2c5942ff3bc849f273cd8fb
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2020-03-12 22:27:27 +0000

    fixed var issue

--- components/cftapps_cluster_lb_backend/main.tf
+++ components/cftapps_cluster_lb_backend/main.tf
@@ -1,10 +1,10 @@
 locals {
-  envs = "${(var.env == "aat")? "stg" : "${(var.env == "perftest")? "test" : "${var.env}"}" }"
+  env = "${(var.env == "aat")? "stg" : "${(var.env == "perftest")? "test" : "${var.env}"}" }"
 }
 
 module "app-gw" {
   source    = "../../modules/app-gateway-module-backend"
-  yaml_path = "${path.cwd}/../../environments/${var.env}/all.yaml"
+  yaml_path = "${path.cwd}/../../environments/${local.env}/all.yaml"
   env       = var.env
 
   location           = var.location

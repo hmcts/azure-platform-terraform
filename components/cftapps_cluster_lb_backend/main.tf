f2cbc8c3bdbceeb512e6c70014ff24e53ab83db8
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2020-03-13 11:16:51 +0000

    renamed backend cfg files and added missing demo tfvars

--- components/cftapps_cluster_lb_backend/main.tf
+++ components/cftapps_cluster_lb_backend/main.tf
@@ -4,7 +4,7 @@ locals {
 
 module "app-gw" {
   source    = "../../modules/app-gateway-module-backend"
-  yaml_path = "${path.cwd}/../../environments/${local.env}/all.yaml"
+  yaml_path = "${path.cwd}/../../environments/${local.env}/backend_lb_config.yaml"
   env       = var.env
 
   location           = var.location

0af5932fdb0b75484b937af7ca7f0a225c6dcb5a
Teo <35260759+tpetkovic@users.noreply.github.com>
2020-06-26 16:09:03 +0100

    Update nonprod.tf

--- components/dynatrace_activegate/nonprod.tf
+++ components/dynatrace_activegate/nonprod.tf
@@ -1,9 +1,9 @@
 module "nonprod_dynatrace_activegate" {
-  source         = "../../modules/dynatrace-activegate"
-  instance_count = 2
+  source                  = "../../modules/dynatrace-activegate"
+  instance_count          = 2
   dynatrace_instance_name = "yrk32651"
-  env = "nonprod"
-  network_zone = "azure_cft"
+  env                     = "nonprod"
+  network_zone            = "azure_cft"
 
   common_tags = local.common_tags
 }

102af8456e57f97bfda98883ed4501507b08d014
Henry Dobson <henrydobson@me.com>
2020-07-14 16:44:42 +0100

    test(tf): try

--- modules/app-gateway-module/main.tf
+++ modules/app-gateway-module/main.tf
@@ -74,7 +74,7 @@ resource "azurerm_application_gateway" "ag" {
   dynamic "backend_http_settings" {
     for_each = [for app in var.frontends : {
       name                  = app.name
-      cookie_based_affinity = contains(tolist(keys(app)), "appgw_cookie_based_affinity") ? app.appgw_cookie_based_affinity : "Disabled"
+      cookie_based_affinity = try(tostring(app.appgw_cookie_based_affinity), "Disabled")
     }]
 
     content {

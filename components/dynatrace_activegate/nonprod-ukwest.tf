ba33d99f73bdfb37435b73cf4f2dcd43bd5ee9cf
Luigi Bitonti <luigi.bitonti@hmcts.net>
2020-09-10 14:00:32 +0100

    Dynatrace nonprod active gate UK West

---
+++ components/dynatrace_activegate/nonprod-ukwest.tf
@@ -0,0 +1,10 @@
+module "nonprod_dynatrace_activegate" {
+  source                  = "../../modules/dynatrace-activegate"
+  instance_count          = 1
+  dynatrace_instance_name = "yrk32651"
+  env                     = "nonprod"
+  network_zone            = "azure.cft"
+  location                = "UK West"
+
+  common_tags = local.common_tags
+}

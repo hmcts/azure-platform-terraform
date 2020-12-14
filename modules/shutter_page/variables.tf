d2a6f56f5205a8e3de33dbed1619e8d978559f1a
Thomas Thornton <>
2020-12-14 11:30:31 +0000

    remove var

--- modules/shutter_page/variables.tf
+++ modules/shutter_page/variables.tf
@@ -2,7 +2,6 @@ variable "resource_group_name" {}
 variable "env" {}
 variable "common_tags" {}
 variable "certificate_key_vault_name" {}
-variable "cdn_profile" {}
 variable "subscription" {}
 variable "shutter_apps" {
   default = []

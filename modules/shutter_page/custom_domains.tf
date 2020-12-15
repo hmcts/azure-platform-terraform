41332f542ebf06be29dc4a4168c4efd1cd8e895d
Thomas Thornton <>
2020-12-15 17:47:43 +0000

    To remove if frontend.name != "jui-redirect" from custom domain & enable https due to known issue.

--- modules/shutter_page/custom_domains.tf
+++ modules/shutter_page/custom_domains.tf
@@ -3,7 +3,7 @@ data "template_file" "customdomain" {
 }
 
 resource "azurerm_template_deployment" "custom_domain" {
-  for_each = { for frontend in var.shutter_apps : frontend.name => frontend
+  for_each = { for frontend in var.shutter_apps : frontend.name => frontend if frontend.name != "jui-redirect"
   }
 
   template_body       = data.template_file.customdomain.rendered

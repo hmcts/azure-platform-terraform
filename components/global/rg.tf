16bddbeeb922e0b8886fa819e2fffe99216c1974
Matt Slater <matt.slater@hmcts.net>
2021-01-08 12:10:49 +0000

    DTSPO-75 - comment out data source

--- components/global/rg.tf
+++ components/global/rg.tf
@@ -6,8 +6,8 @@ data "azurerm_resource_group" "key_vault" {
   name = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
 }
 
-data "azurerm_log_analytics_workspace" "log" {
-  provider            = azurerm.loganalytics
-  name                = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].name
-  resource_group_name = "oms-automation"
-}
+# data "azurerm_log_analytics_workspace" "log" {
+#   provider            = azurerm.loganalytics
+#   name                = module.log_analytics_workspace.name #local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].name
+#   resource_group_name = "oms-automation"
+# }

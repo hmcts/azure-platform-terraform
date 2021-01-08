5dfd96731621465b0db1bbe01db4fa130cfb9c4e
Matt Slater <matt.slater@hmcts.net>
2021-01-08 13:05:56 +0000

    DTSPO-75 - tf fmt

--- components/global/log_analytics.tf
+++ components/global/log_analytics.tf
@@ -1,4 +1,4 @@
 module "log_analytics_workspace" {
-  source = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
   environment = var.env
 }

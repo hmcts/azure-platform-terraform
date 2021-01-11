444efe986e0369be75107b9d29dd2f0ee78075e2
Matt Slater <matt.slater@hmcts.net>
2021-01-11 10:04:46 +0000

    DTSPO-75 - remove commented code

--- components/global/provider.tf
+++ components/global/provider.tf
@@ -20,10 +20,3 @@ provider "azurerm" {
   features {}
   subscription_id = var.data_subscription
 }
-
-# provider "azurerm" {
-#   features {}
-#   alias           = "loganalytics"
-#   subscription_id = module.log_analytics_workspace.subscription_id
-#   # subscription_id = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].subscription_id
-# }

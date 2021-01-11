28955ec7d758dc1bf706351be3bbf98365e2cc69
Matt Slater <matt.slater@hmcts.net>
2021-01-11 09:32:11 +0000

    DTSPO-75 - tidy up

--- components/global/local.tf
+++ components/global/local.tf
@@ -31,26 +31,4 @@ locals {
     "environment"        = local.env_display_names[var.env]
     "criticality"        = local.criticality[var.env]
   }
-
-
-  # log_analytics_env_mapping = {
-  #   sandbox = ["sbox"]
-  #   nonprod = ["dev", "test", "ithc", "demo", "stg"]
-  #   prod    = ["prod", "mgmt"]
-  # }
-  # log_analytics_workspace = {
-  #   sandbox = {
-  #     subscription_id = "bf308a5c-0624-4334-8ff8-8dca9fd43783"
-  #     name            = "hmcts-sandbox"
-  #   }
-  #   nonprod = {
-  #     subscription_id = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
-  #     name            = "hmcts-nonprod"
-  #   }
-  #   prod = {
-  #     subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
-  #     name            = "hmcts-prod"
-  #   }
-  # }
-
 }

c9aa1ec18266b76fccc99b69e0dd68222674301e
Matt Slater <matt.slater@hmcts.net>
2021-01-11 21:54:07 +0000

    DTSPO-75 - set frontdoor module's branch to master

--- components/global/main.tf
+++ components/global/main.tf
@@ -1,5 +1,5 @@
 module "landing_zone" {
-  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=DTSPO-429-remove-provider"
+  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=master"
 
   providers = {
     azurerm      = azurerm

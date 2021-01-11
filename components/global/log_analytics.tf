4d4828d37e0e7efe121caeef7916f4241e204d77
Matt Slater <matt.slater@hmcts.net>
2021-01-11 09:09:59 +0000

    DTSPO-75 - update workspace id branch to handle new workspaces

--- components/global/log_analytics.tf
+++ components/global/log_analytics.tf
@@ -1,4 +1,4 @@
 module "log_analytics_workspace" {
-  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=DTSPO-75-add-more-workspaces"
   environment = var.env
 }

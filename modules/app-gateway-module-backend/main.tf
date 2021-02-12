7d989534d10dc1420f2a5e1eadb9f5a84d3d3cb4
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2021-02-12 09:55:12 +0000

    make component optional for backends

--- modules/app-gateway-module-backend/main.tf
+++ modules/app-gateway-module-backend/main.tf
@@ -64,7 +64,7 @@ resource "azurerm_application_gateway" "ag" {
 
   dynamic "backend_address_pool" {
     for_each = [for app in local.gateways[count.index].app_configuration : {
-      name = "${app.product}-${app.component}"
+      name = app.component != "" ? "${app.product}-${app.component}" : "${app.product}"
     }]
 
     content {
@@ -75,10 +75,10 @@ resource "azurerm_application_gateway" "ag" {
 
   dynamic "probe" {
     for_each = [for app in local.gateways[count.index].app_configuration : {
-      name          = "${app.product}-${app.component}"
+      name          = app.component != "" ? "${app.product}-${app.component}" : "${app.product}"
       path          = lookup(app, "health_path_override", "/health/liveness")
-      host_name     = "${app.product}-${app.component}-${var.env}.${local.gateways[count.index].gateway_configuration.host_name_suffix}"
-      ssl_host_name = "${app.product}-${app.component}.${local.gateways[count.index].gateway_configuration.ssl_host_name_suffix}"
+      host_name     = "${name}-${var.env}.${local.gateways[count.index].gateway_configuration.host_name_suffix}"
+      ssl_host_name = "${name}.${local.gateways[count.index].gateway_configuration.ssl_host_name_suffix}"
       ssl_enabled   = contains(keys(app), "ssl_enabled") ? app.ssl_enabled : false
     }]
 
@@ -95,7 +95,7 @@ resource "azurerm_application_gateway" "ag" {
 
   dynamic "backend_http_settings" {
     for_each = [for app in local.gateways[count.index].app_configuration : {
-      name                  = "${app.product}-${app.component}"
+      name                  = app.component != "" ? "${app.product}-${app.component}" : "${app.product}"
       cookie_based_affinity = contains(keys(app), "cookie_based_affinity") ? app.cookie_based_affinity : "Disabled"
     }]
 
@@ -117,9 +117,9 @@ resource "azurerm_application_gateway" "ag" {
 
   dynamic "http_listener" {
     for_each = [for app in local.gateways[count.index].app_configuration : {
-      name                 = "${app.product}-${app.component}"
-      host_name            = "${app.product}-${app.component}-${var.env}.${local.gateways[count.index].gateway_configuration.host_name_suffix}"
-      ssl_host_name        = "${app.product}-${app.component}.${local.gateways[count.index].gateway_configuration.ssl_host_name_suffix}"
+      name                 = app.component != "" ? "${app.product}-${app.component}" : "${app.product}"
+      host_name            = "${name}-${var.env}.${local.gateways[count.index].gateway_configuration.host_name_suffix}"
+      ssl_host_name        = "${name}.${local.gateways[count.index].gateway_configuration.ssl_host_name_suffix}"
       ssl_enabled          = contains(keys(app), "ssl_enabled") ? app.ssl_enabled : false
       ssl_certificate_name = "${local.gateways[count.index].gateway_configuration.certificate_name}"
     }]
@@ -136,7 +136,7 @@ resource "azurerm_application_gateway" "ag" {
 
   dynamic "request_routing_rule" {
     for_each = [for app in local.gateways[count.index].app_configuration : {
-      name = "${app.product}-${app.component}"
+      name = app.component != "" ? "${app.product}-${app.component}" : "${app.product}"
     }]
 
     content {

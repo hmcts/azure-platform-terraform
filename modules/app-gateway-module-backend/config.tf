29f0be62276a6b1243569ffa7e4dfd16b6cd8df3
Thomas Thornton <>
2021-03-01 16:10:16 +0000

    terraform fmt

--- modules/app-gateway-module-backend/config.tf
+++ modules/app-gateway-module-backend/config.tf
@@ -42,7 +42,7 @@ data "azurerm_subnet" "app_gw" {
 
 resource "azurerm_public_ip" "app_gw" {
   count               = length(var.private_ip_address)
-  name                = element(var.private_ip_address, count.index) == var.private_ip_address[0]  ? "aks-appgw-${var.env}-pip" : "aks-appgw-${var.env}-pip-${count.index}"
+  name                = element(var.private_ip_address, count.index) == var.private_ip_address[0] ? "aks-appgw-${var.env}-pip" : "aks-appgw-${var.env}-pip-${count.index}"
   location            = var.location
   resource_group_name = local.vnet_rg
   sku                 = "Standard"
--- modules/app-gateway-module-backend/secret.tf
+++ modules/app-gateway-module-backend/secret.tf
@@ -5,7 +5,7 @@ data "azurerm_key_vault" "main" {
 
 resource "azurerm_key_vault_secret" "test" {
   count        = length(var.private_ip_address)
-  name         = element(var.private_ip_address, count.index) == var.private_ip_address[0]  ? "internal-lb-ip" : "internal-lb-ip-${count.index}"
+  name         = element(var.private_ip_address, count.index) == var.private_ip_address[0] ? "internal-lb-ip" : "internal-lb-ip-${count.index}"
   value        = var.private_ip_addresses
   key_vault_id = data.azurerm_key_vault.main.id
 

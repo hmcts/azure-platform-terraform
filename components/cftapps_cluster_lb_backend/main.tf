4f3cfcd8304f710bd690e8cc0d6eaadcc16565bd
Darren Bromwich <d.bromwich@kainos.com>
2021-03-03 13:42:44 +0000

    added log analytics workspace id to app-gw

--- components/cftapps_cluster_lb_backend/main.tf
+++ components/cftapps_cluster_lb_backend/main.tf
@@ -13,13 +13,15 @@ module "app-gw" {
   yaml_path = "${path.cwd}/../../environments/${local.env}/backend_lb_config.yaml"
   env       = var.env
 
-  location                  = var.location
-  private_ip_address        = var.app_gw_private_ip_address
-  backend_pool_ip_addresses = var.cft_apps_cluster_ips
-  subscription              = var.subscription
-  vault_name                = var.certificate_key_vault_name
-  oms_env                   = var.oms_env
-  vnet_rg                   = local.vnet_rg
-  vnet_name                 = local.vnet_name
-  common_tags               = local.common_tags
+  location                   = var.location
+  private_ip_address         = var.app_gw_private_ip_address
+  backend_pool_ip_addresses  = var.cft_apps_cluster_ips
+  subscription               = var.subscription
+  vault_name                 = var.certificate_key_vault_name
+  oms_env                    = var.oms_env
+  vnet_rg                    = local.vnet_rg
+  vnet_name                  = local.vnet_name
+  common_tags                = local.common_tags
+  log_analytics_workspace_id = module.logworkspace.workspace_id
+
 }

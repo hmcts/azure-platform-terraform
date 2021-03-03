910de766f22e569898ea252933fcf59458299179
Darren Bromwich <d.bromwich@kainos.com>
2021-03-03 14:43:45 +0000

    adding missing locals

--- components/cftapps_cluster_lb_backend/main.tf
+++ components/cftapps_cluster_lb_backend/main.tf
@@ -1,8 +1,47 @@
 locals {
-  env                      = (var.env == "aat") ? "stg" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
+  env = (var.env == "aat") ? "stg" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
+
+  criticality = {
+    sbox     = "Low"
+    aat      = "High"
+    prod     = "High"
+    ithc     = "Medium"
+    perftest = "Medium"
+    ldata    = "Medium"
+
+  }
+
+  env_display_names = {
+    sbox     = "Sandbox"
+    aat      = "Staging"
+    prod     = "Production"
+    ithc     = "ITHC"
+    perftest = "Test"
+    ldata    = "LDATA"
+  }
+
+  common_tags = {
+    "managedBy"          = "Platform Operations"
+    "solutionOwner"      = "CFT"
+    "activityName"       = "AKS"
+    "dataClassification" = "Internal"
+    "automation"         = ""
+    "costCentre"         = "10245117" // until we get a better one, this is the generic cft contingency one
+    "environment"        = local.env_display_names[var.env]
+    "criticality"        = local.criticality[var.env]
+  }
+
+  vnet_rg   = "aks-infra-${var.env}-rg"
+  vnet_name = "core-${var.env}-vnet"
+
   key_vault_resource_group = var.env == "perftest" || var.env == "aat" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
 }
 
+module "logworkspace" {
+  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  environment = var.env
+
+}
 module "app-gw" {
   source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=DTSPO-1355"
 

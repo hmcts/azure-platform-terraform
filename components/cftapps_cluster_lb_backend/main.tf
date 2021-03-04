b70fdbe4cade49f626f2d9adeeb23393fa194d0c
Darren Bromwich <d.bromwich@kainos.com>
2021-03-04 09:02:21 +0000

    added log workspace

--- components/cftapps_cluster_lb_backend/main.tf
+++ components/cftapps_cluster_lb_backend/main.tf
@@ -38,6 +38,11 @@ locals {
   key_vault_resource_group = var.env == "perftest" || var.env == "aat" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
 }
 
+module "logworkspace" {
+  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  environment = var.env
+
+}
 module "app-gw" {
   source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=DTSPO-1355"
 
@@ -49,15 +54,16 @@ module "app-gw" {
   yaml_path = "${path.cwd}/../../environments/${local.env}/backend_lb_config.yaml"
   env       = var.env
 
-  location                  = var.location
-  private_ip_address        = var.app_gw_private_ip_address
-  backend_pool_ip_addresses = var.cft_apps_cluster_ips
-  subscription              = var.subscription
-  vault_name                = var.certificate_key_vault_name
-  oms_env                   = var.oms_env
-  vnet_rg                   = local.vnet_rg
-  vnet_name                 = local.vnet_name
-  common_tags               = local.common_tags
-  key_vault_resource_group  = local.key_vault_resource_group
+  location                   = var.location
+  private_ip_address         = var.app_gw_private_ip_address
+  backend_pool_ip_addresses  = var.cft_apps_cluster_ips
+  subscription               = var.subscription
+  vault_name                 = var.certificate_key_vault_name
+  oms_env                    = var.oms_env
+  vnet_rg                    = local.vnet_rg
+  vnet_name                  = local.vnet_name
+  common_tags                = local.common_tags
+  log_analytics_workspace_id = module.logworkspace.workspace_id
+  key_vault_resource_group   = local.key_vault_resource_group
 
 }

c53779d1be44f792f5f13792edd0dcae30287f9e
Darren Bromwich <d.bromwich@kainos.com>
2021-03-11 12:00:24 +0000

    added providers

---
+++ components/private_dns/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 0.13.0"
+
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+provider "azurerm" {
+  alias = "data"
+  features {}
+  subscription_id = var.data_subscription
+}
+
+provider "local" {
+  version = "=1.3.0"
+}

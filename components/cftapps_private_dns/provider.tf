89c8fccffc0e6ef11e309b1cef34a19c5f12b85f
Thomas Thornton <>
2021-03-18 20:14:57 +0000

    no message

--- components/cftapps_private_dns/provider.tf
+++ components/cftapps_private_dns/provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_version = ">= 0.13.0"
-  backend "azurerm" {
+
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.41.0"
+    }
   }
 }
-
 provider "azurerm" {
   features {}
   skip_provider_registration = true

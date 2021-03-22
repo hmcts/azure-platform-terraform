0528582b917d16b5ccf90d2deb81051aa0e7cede
Thomas Thornton <>
2021-03-22 08:48:18 +0000

    Update terraform import privatedns script

--- components/cftapps_private_dns/scripts/tf_import_dns.sh
+++ components/cftapps_private_dns/scripts/tf_import_dns.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
-# Example 
-# ENV="sbox"
-# SUBSCRIPTION_ID="123-123-123-123"
-# RESOURCE_GROUP="tamops-sbox-rg"
-# PRIVATE_DNS_ZONE="service.core-compute-tamops.internal"
+
+# Example
+# ENV="ithc"
+# SUBSCRIPTION_ID="1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+# RESOURCE_GROUP="core-infra-intsvc-rg"
+# PRIVATE_DNS_ZONE="service.core-compute-ithc.internal"
+
 ENV=""
 SUBSCRIPTION_ID=""
 RESOURCE_GROUP=""
@@ -12,14 +14,21 @@ PRIVATE_DNS_ZONE=""
 
 PRODUCTS=()
 COMPONENTS=()
-PRODUCTS+=($(yq e '.gateways[].app_configuration[].product' "../../environments/sbox/backend_lb_config.yaml"))
-COMPONENTS+=($(yq e '.gateways[].app_configuration[].component' "../../environments/sbox/backend_lb_config.yaml"))
+PRODUCTS+=($(yq e '.gateways[].app_configuration[].product' "../../environments/ithc/backend_lb_config.yaml"))
+COMPONENTS+=($(yq e '.gateways[].app_configuration[].component' "../../environments/ithc/backend_lb_config.yaml"))
 
 for i in "${!PRODUCTS[@]}"; do
 
-product_component=("${PRODUCTS[i]}"-"${COMPONENTS[i]}"-"$ENV")
+PRODUCT_COMPONENT=("${PRODUCTS[i]}"-"${COMPONENTS[i]}"-"$ENV")
+RESOURCE=()
 
-terraform import module.privatedns.azurerm_private_dns_a_record.this[\"$product_component\"]  /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Network/privateDnsZones/$PRIVATE_DNS_ZONE/A/$product_component
+RESOURCE=($(az network private-dns record-set a list -g core-infra-intsvc-rg -z service.core-compute-ithc.internal --query "[?id=='/subscriptions/$SUBSCRIPTION_ID/RESOURCEGroups/$RESOURCE_GROUP/providers/Microsoft.Network/privateDnsZones/$PRIVATE_DNS_ZONE/A/$PRODUCT_COMPONENT']" -o tsv))
+
+if [ -n "$RESOURCE" ]
+then
+    terraform import module.privatedns.azurerm_private_dns_a_record.this[\"$PRODUCT_COMPONENT\"]  /subscriptions/$SUBSCRIPTION_ID/RESOURCEGroups/$RESOURCE_GROUP/providers/Microsoft.Network/privateDnsZones/$PRIVATE_DNS_ZONE/A/$PRODUCT_COMPONENT
+else
+    echo $PRODUCT_COMPONENT has no record in $PRIVATE_DNS_ZONE to import
+fi
 
 done
-

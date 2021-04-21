27fcfb49e419a9ef25dcc0b04b715bb5ab4fb656
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-04-21 19:36:39 +0100

    replaced common tags for shutter

--- components/shutter/module_shutter.tf
+++ components/shutter/module_shutter.tf
@@ -3,9 +3,16 @@ module "shutter" {
   resource_group_name        = var.shutter_rg
   env                        = var.env
   shutter_apps               = var.frontends
-  common_tags                = local.common_tags
+  common_tags                = module.ctags.common_tags
   certificate_key_vault_name = var.certificate_key_vault_name
   subscription               = var.subscription
   shutter_rg                 = var.shutter_rg
   cdn_sku                    = var.cdn_sku
+}
+
+module "ctags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.env
+  product     = var.product
+  builtFrom   = var.builtFrom
 }

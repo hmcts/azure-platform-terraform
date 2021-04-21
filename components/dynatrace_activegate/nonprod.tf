0ab9b5d70a9947ae33ea4bda5517d967d8309d54
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-04-21 19:29:32 +0100

    added tag module

--- components/dynatrace_activegate/nonprod.tf
+++ components/dynatrace_activegate/nonprod.tf
@@ -7,3 +7,10 @@ module "nonprod_dynatrace_activegate" {
 
   common_tags = module.ctags.common_tags
 }
+
+module "ctags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.env
+  product     = var.product
+  builtFrom   = var.builtFrom
+}
--- components/dynatrace_activegate/prod.tf
+++ components/dynatrace_activegate/prod.tf
@@ -5,3 +5,10 @@ module "dynatrace_activegate" {
 
   common_tags = module.ctags.common_tags
 }
+
+module "ctags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.env
+  product     = var.product
+  builtFrom   = var.builtFrom
+}

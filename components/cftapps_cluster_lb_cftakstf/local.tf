6847a97974d3eea7292befa76bae43ba3b8e287a
Thomas Thornton <>
2021-05-13 08:36:08 +0100

    Update

--- components/cftapps_cluster_lb_cftakstf/local.tf
+++ components/cftapps_cluster_lb_cftakstf/local.tf
@@ -1,4 +1,6 @@
 locals {
-  vnet_rg   = "cft-${var.env}-network-rg"
-  vnet_name = "cft-${var.env}-vnet"
+  env = (var.env == "aat") ? "stg" : (var.env == "sbox_cft_akstf") ? "sbox" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
+
+  vnet_rg   = "cft-${local.env}-network-rg"
+  vnet_name = "cft-${local.env}-vnet"
 }

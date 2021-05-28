447b2369cfd77e21887c450cffeeeeff659e5229
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-05-28 13:17:36 +0100

    [ignore] testing frontdoor managed cert redirect fix

--- components/global/main.tf
+++ components/global/main.tf
@@ -1,5 +1,5 @@
 module "landing_zone" {
-  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=master"
+  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=fix-redirect-managed-cert"
 
   common_tags                = module.ctags.common_tags
   env                        = var.env

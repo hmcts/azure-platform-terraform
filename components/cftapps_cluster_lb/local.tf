8c0577ac6b3985eaae9df593de9590bd9b225b61
Tim Jacomb <tim.jacomb@hmcts.net>
2021-06-01 11:46:47 +0100

    fmt

--- components/cftapps_cluster_lb/local.tf
+++ components/cftapps_cluster_lb/local.tf
@@ -2,6 +2,6 @@ locals {
   vnet_rg   = var.env == "sbox" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
   vnet_name = var.env == "sbox" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
 
-  potential_kv_rg = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
+  potential_kv_rg          = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
   key_vault_resource_group = var.env == "sbox" ? "cft-platform-${var.env}-rg" : local.potential_kv_rg
 }

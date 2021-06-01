ac00f1f02c10c7bea1bbe6bdba7dfd830b6163e1
Tim Jacomb <tim.jacomb@hmcts.net>
2021-06-01 11:41:02 +0100

    Override kv rg

--- components/global/rg.tf
+++ components/global/rg.tf
@@ -1,7 +1,12 @@
+locals {
+  potential_kv_rg = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
+  key_vault_resource_group = var.env == "sbox" ? "cft-platform-${var.env}-rg" : local.potential_kv_rg
+}
+
 data "azurerm_resource_group" "main" {
   name = "lz-${var.env}-rg"
 }
 
 data "azurerm_resource_group" "key_vault" {
-  name = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
+  name = local.key_vault_resource_group
 }

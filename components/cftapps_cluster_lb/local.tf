7adad4e2fc4e90e9e801a9c327ad59013d95f864
Thomas Thornton <>
2021-06-01 11:53:45 +0100

    fmt

--- components/cftapps_cluster_lb/local.tf
+++ components/cftapps_cluster_lb/local.tf
@@ -2,5 +2,5 @@ locals {
   vnet_rg   = var.env == "sbox" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
   vnet_name = var.env == "sbox" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
 
-  potential_kv_rg          = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
+  potential_kv_rg = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
 }

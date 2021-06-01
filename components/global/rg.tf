41f9e8ea431a8dc40442ca1f65f05a191529e668
Thomas Thornton <>
2021-06-01 11:44:19 +0100

    fmt

--- components/global/rg.tf
+++ components/global/rg.tf
@@ -1,5 +1,5 @@
 locals {
-  potential_kv_rg = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
+  potential_kv_rg          = var.env == "perftest" || var.env == "aat" || var.env == "preview" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"
   key_vault_resource_group = var.env == "sbox" ? "cft-platform-${var.env}-rg" : local.potential_kv_rg
 }
 

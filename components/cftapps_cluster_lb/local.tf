123831262d983d5e7ef68b82d3d5793c0509e0ad
Thomas Thornton <>
2021-06-01 09:27:43 +0100

    Typo

--- components/cftapps_cluster_lb/local.tf
+++ components/cftapps_cluster_lb/local.tf
@@ -1,5 +1,5 @@
 locals {
-  vnet_rg   = var.env == "sbox" ? "cft-${var.env}-vnet" : "aks-infra-${var.env}-rg"
+  vnet_rg   = var.env == "sbox" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
   vnet_name = var.env == "sbox" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
 
   key_vault_resource_group = var.env == "sbox" ? "genesis-rg" : var.env == "perftest" || var.env == "aat" ? "core-infra-${var.subscription}-rg" : "core-infra-${var.env}-rg"

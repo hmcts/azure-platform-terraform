542b7d33bef75d8597f2dc0f1d7452d750060426
Thomas Thornton <>
2021-06-28 15:37:33 +0100

    Removing duplicate module reference

--- components/dynatrace_activegate/nonprod-private.tf
+++ components/dynatrace_activegate/nonprod-private.tf
@@ -9,10 +9,3 @@ module "nonprod_dynatrace_activegate_private" {
 
   common_tags = module.ctags.common_tags
 }
-
-module "ctags" {
-  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = var.env
-  product     = var.product
-  builtFrom   = var.builtFrom
-}

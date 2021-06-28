11ee35d7b4aa98136077e7999bdc16b076416be5
Thomas Thornton <>
2021-06-28 15:28:35 +0100

    Update to correct source

--- components/dynatrace_activegate/nonprod-private.tf
+++ components/dynatrace_activegate/nonprod-private.tf
@@ -1,6 +1,6 @@
 # For private ActiveGate synthethic monitoring
 module "nonprod_dynatrace_activegate_private" {
-  source                  = "../../modules/dynatrace-private"
+  source                  = "../../modules/dynatrace-activegate"
   instance_count          = 2
   dynatrace_instance_name = "yrk32651"
   env                     = "nonprod"

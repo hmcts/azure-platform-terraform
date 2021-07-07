a33ea1f7319940750021f8b0fce1b18334776049
Thomas Thornton <>
2021-07-07 09:35:17 +0100

    provider check

--- components/dynatrace_activegate/provider.tf
+++ components/dynatrace_activegate/provider.tf
@@ -4,8 +4,9 @@ terraform {
   backend "azurerm" {}
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.58.0"
+      source                = "hashicorp/azurerm"
+      version               = "2.58.0"
+      configuration_aliases = [azurerm.law]
     }
   }
 }

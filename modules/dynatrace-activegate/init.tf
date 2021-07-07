23d537fce66cd56ad22669d65ecfe83209b4f547
Thomas Thornton <>
2021-07-07 09:52:34 +0100

    no message

--- modules/dynatrace-activegate/init.tf
+++ modules/dynatrace-activegate/init.tf
@@ -1,11 +1,7 @@
 terraform {
-  required_version = ">= 0.13.0"
-
-  backend "azurerm" {}
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "2.58.0"
       configuration_aliases = [azurerm.law]
     }
   }

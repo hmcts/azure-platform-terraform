cb14f5078ef46ed38fc2c16be75431e731a323e7
Thomas Thornton <>
2021-07-07 09:45:08 +0100

    Swap to module

--- components/dynatrace_activegate/provider.tf
+++ components/dynatrace_activegate/provider.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_version = ">= 0.13.0"
-
-  backend "azurerm" {}
-  required_providers {
-    azurerm = {
-      source                = "hashicorp/azurerm"
-      version               = "2.58.0"
-      configuration_aliases = [azurerm.law]
-    }
-  }
-}
 provider "azurerm" {
   features {}
   skip_provider_registration = true

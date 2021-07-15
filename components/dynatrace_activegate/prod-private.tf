ec2d6f16ebf7af8c1e4eac9210e3af7cdbb27425
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-07-15 10:28:28 +0100

    terraform fmt

--- components/dynatrace_activegate/prod-private.tf
+++ components/dynatrace_activegate/prod-private.tf
@@ -6,11 +6,11 @@ module "prod_dynatrace_activegate_private" {
     azurerm.law = azurerm.law_prod
   }
 
-  source           = "../../modules/dynatrace-activegate"
-  instance_count   = 2
-  network_zone     = "azure.cft"
-  config_file_name = "cloudconfig-private"
-  enable_log_analytics    = true
+  source               = "../../modules/dynatrace-activegate"
+  instance_count       = 2
+  network_zone         = "azure.cft"
+  config_file_name     = "cloudconfig-private"
+  enable_log_analytics = true
 
   common_tags = module.ctags.common_tags
 }

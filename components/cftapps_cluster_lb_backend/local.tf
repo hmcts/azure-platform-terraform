309d149dcce05876483be4e3e3732ee44b6fd677
Darren Bromwich <d.bromwich@kainos.com>
2021-08-12 11:14:00 +0100

    updated to locals

--- components/cftapps_cluster_lb_backend/local.tf
+++ components/cftapps_cluster_lb_backend/local.tf
@@ -1,8 +1,8 @@
 locals {
-  env = (var.env == "aat") ? "stg" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
+  env = (var.env == "aat") ? "stg" : (var.env == "ithc") ? "ithc" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
 
-  vnet_rg   = var.env == "sbox" || var.env == "perftest" || var.env == "aat" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
-  vnet_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
+  vnet_rg   = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
+  vnet_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
 
   key_vault_resource_group = "cft-platform-${var.subscription}-rg"
 }

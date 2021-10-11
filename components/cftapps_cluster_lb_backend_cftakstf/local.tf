4e8667bf4318a2949856df2158bee191f26268e3
Thomas Thornton <>
2021-10-11 14:15:29 +0100

    Cleanup unused files

--- components/cftapps_cluster_lb_backend_cftakstf/local.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  env = (var.env == "aat") ? "stg" : (var.env == "sbox_cft_akstf") ? "sbox" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
-
-  vnet_rg   = "cft-${local.env}-network-rg"
-  vnet_name = "cft-${local.env}-vnet"
-
-  key_vault_resource_group = "cft-platform-${var.subscription}-rg"
-}
--- components/cftapps_cluster_lb_cftakstf/local.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  env = (var.env == "aat") ? "stg" : (var.env == "sbox_cft_akstf") ? "sbox" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
-
-  vnet_rg   = "cft-${local.env}-network-rg"
-  vnet_name = "cft-${local.env}-vnet"
-}

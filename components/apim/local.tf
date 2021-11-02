246b3e84b475eaff72f4324f0416c0c1777251f3
Thomas Thornton <>
2021-11-02 09:24:33 +0000

    whoops

--- components/apim/local.tf
+++ components/apim/local.tf
@@ -1,4 +1,4 @@
 locals {
-  vnet_rg   = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
-  vnet_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
+  vnet_rg   = "core-infra-${var.env}"
+  vnet_name = "core-infra-vnet-${var.env}"
 }

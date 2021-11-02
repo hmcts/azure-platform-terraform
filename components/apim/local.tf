0e1d3de33fdbe1187b03f7323a1923ddcf93eec3
Thomas Thornton <>
2021-11-02 09:40:09 +0000

    Typo

--- components/apim/local.tf
+++ components/apim/local.tf
@@ -1,5 +1,5 @@
 locals {
-  env       = var.environment == "sbox" ? "sandbox" : var.environment == "test" ? "perftest" : var.environment == "aat" ? "staging" : "${var.env}"
+  env       = var.env == "sbox" ? "sandbox" : var.env == "test" ? "perftest" : var.env == "aat" ? "staging" : "${var.env}"
   vnet_rg   = "core-infra-${local.env}"
   vnet_name = "core-infra-vnet-${local.env}"
 }

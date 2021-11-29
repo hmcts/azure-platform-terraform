b58e37b5171f9ac001f8ad6c712ecf31b9b6956d
chrisorisawayi <chris.orisawayi@hmcts.net>
2021-11-29 13:27:49 +0000

    testing apim private module branch update

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -6,7 +6,7 @@ module "ctags" {
 }
 
 module "api-mgmt" {
-  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=dtspo-4829-apim-private-perftest"
   location                       = var.location
   sku_name                       = var.apim_sku_name
   virtual_network_resource_group = local.vnet_rg

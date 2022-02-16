41909c2978299983290f9d2a15e41dfda475f601
chrisorisawayi <chris.orisawayi@hmcts.net>
2022-02-16 11:58:47 +0000

    create test-apim-terraform folder

---
+++ components/apim-terraform/main.tf
@@ -0,0 +1,27 @@
+module "ctags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.env
+  product     = var.product
+  builtFrom   = var.builtFrom
+}
+
+module "api-mgmt" {
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
+  location                       = var.location
+  sku_name                       = var.apim_sku_name
+  virtual_network_resource_group = local.vnet_rg
+  virtual_network_name           = local.vnet_name
+  environment                    = var.env
+  virtual_network_type           = "Internal"
+  department                     = var.department
+  common_tags                    = module.ctags.common_tags
+  route_next_hop_in_ip_address   = local.hub[var.hub].ukSouth.next_hop_ip
+}
+
+resource "azurerm_api_management_named_value" "environment" {
+  name                = "environment"
+  resource_group_name = local.vnet_rg
+  api_management_name = module.api-mgmt.name
+  display_name        = "environment"
+  value               = var.env
+}
---
+++ components/apim-terraform/variables.tf
@@ -0,0 +1,108 @@
+// terraform doesn't let you have undeclared vars used from a tfvars file but no variable declaration
+// we link this file into each component
+// from the component dir: `ln -s ../../environments/variables.tf variables.tf`
+
+variable "subscription" {
+  default = []
+}
+
+variable "env" {
+  default = []
+}
+
+variable "project" {
+  default = "hmcts"
+}
+
+variable "product" {
+}
+
+variable "builtFrom" {
+}
+
+variable "shutter_rg" {
+  default = "TODO"
+}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "frontends" {
+  default = []
+}
+
+variable "shutter_storage" {
+  default = "TODO" // Is this even used?
+}
+variable "default_shutter" {
+  default = []
+}
+
+variable "cdn_sku" {
+  default = "Standard_Verizon"
+}
+
+variable "ssl_mode" {
+  default = "FrontDoor"
+}
+
+variable "cft_apps_cluster_ips" {
+  default = []
+}
+variable "cft_apps_ag_ip_address" {
+  default = []
+}
+variable "sscssya_shutter" {
+  default = []
+}
+variable "sscstya_shutter" {
+  default = []
+}
+variable "sscscor_shutter" {
+  default = []
+}
+
+variable "app_gw_private_ip_address" {
+  default = []
+}
+
+variable "mgmt_vnet_name" {
+  default = "TODO"
+}
+variable "mgmt_rg_name" {
+  default = "TODO"
+}
+variable "kv_rg_name" {
+  default = "TODO"
+}
+
+variable "data_subscription" {}
+
+variable "privatedns_subscription" {
+  default = "TODO"
+}
+variable "oms_env" {}
+
+variable "certificate_name_check" {
+  default = true
+}
+variable "common_tags" {
+  default = []
+}
+
+variable "add_access_policy" {
+  default = true
+}
+
+variable "department" {
+  default = "cft"
+}
+
+variable "apim_sku_name" {
+  default = "Developer"
+}
+
+variable "hub" {
+  default = "sbox"
+}

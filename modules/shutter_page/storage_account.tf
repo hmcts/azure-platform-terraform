539a8d18103be6904887d0a27d4abbab76d6beed
Enda Kelly <enda.kelly@hmcts.net>
2022-06-22 10:27:48 +0100

    add min_tls

--- modules/shutter_page/storage_account.tf
+++ modules/shutter_page/storage_account.tf
@@ -10,6 +10,7 @@ resource "azurerm_storage_account" "shutter_store" {
   account_kind                    = "StorageV2"
   enable_https_traffic_only       = true
   allow_nested_items_to_be_public = false
+  min_tls_version                 = "TLS1_0"
 
   static_website {
     index_document     = "index.html"

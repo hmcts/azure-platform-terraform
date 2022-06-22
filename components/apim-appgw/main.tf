23a5873958afd93e1ef5b319417b7e9b88eb53f2
Enda Kelly <enda.kelly@hmcts.net>
2022-06-22 11:22:35 +0100

    test av zone on appgw and fmt

--- components/apim-appgw/main.tf
+++ components/apim-appgw/main.tf
@@ -27,22 +27,23 @@ module "app-gw" {
     azurerm.kv  = azurerm.kv
   }
 
-  source                     = "git::https://github.com/hmcts/terraform-module-apim-application-gateway.git?ref=terraform-azurerm-v3"
-  yaml_path                  = "${path.cwd}/../../environments/${local.env}/apim_appgw_config.yaml"
-  env                        = local.dns_zone
-  location                   = var.location
-  private_ip_address         = var.hub_app_gw_private_ip_address
-  backend_pool_ip_addresses  = var.apim_appgw_backend_pool_ips
-  backend_pool_fqdns         = var.apim_appgw_backend_pool_fqdns
-  vault_name                 = local.key_vault_name
-  vnet_rg                    = local.vnet_rg
-  vnet_name                  = local.vnet_name
-  common_tags                = module.ctags.common_tags
-  log_analytics_workspace_id = module.logworkspace.workspace_id
-  key_vault_resource_group   = local.key_vault_resource_group
-  subnet_name                = local.subnet_name
-  waf_mode                   = var.waf_mode
-  exclusions                 = var.apim_appgw_exclusions
+  source                             = "git::https://github.com/hmcts/terraform-module-apim-application-gateway.git?ref=terraform-azurerm-v3"
+  yaml_path                          = "${path.cwd}/../../environments/${local.env}/apim_appgw_config.yaml"
+  env                                = local.dns_zone
+  location                           = var.location
+  private_ip_address                 = var.hub_app_gw_private_ip_address
+  backend_pool_ip_addresses          = var.apim_appgw_backend_pool_ips
+  backend_pool_fqdns                 = var.apim_appgw_backend_pool_fqdns
+  vault_name                         = local.key_vault_name
+  vnet_rg                            = local.vnet_rg
+  vnet_name                          = local.vnet_name
+  common_tags                        = module.ctags.common_tags
+  log_analytics_workspace_id         = module.logworkspace.workspace_id
+  key_vault_resource_group           = local.key_vault_resource_group
+  subnet_name                        = local.subnet_name
+  waf_mode                           = var.waf_mode
+  exclusions                         = var.apim_appgw_exclusions
+  enable_multiple_availability_zones = var.enable_multiple_availability_zones
 }
 
 resource "azurerm_route_table" "apim_route_table" {

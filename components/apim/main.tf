7d018e1ed2a0ac779898ec2b204ca4d6f10682dd
Enda Kelly <enda.kelly@hmcts.net>
2022-06-28 10:12:37 +0100

    use master branch for apim

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -6,7 +6,7 @@ module "ctags" {
 }
 
 module "api-mgmt" {
-  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=terraform-azurerm-v3"
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=master"
   location                       = var.location
   sku_name                       = var.apim_sku_name
   virtual_network_resource_group = local.vnet_rg

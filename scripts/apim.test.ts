857e677da2338b29e23019c83547347f8c559868
Enda Kelly <enda.kelly@hmcts.net>
2022-08-17 11:47:43 +0100

    test with branch

---
+++ scripts/apim.test.ts
@@ -0,0 +1,72 @@
+import { fail } from "assert";
+
+import axios, { AxiosResponse } from "axios";
+
+
+const APP_NAME =
+  process.env.APP_NAME
+
+const TEST_URL =
+  process.env.TEST_URL
+
+describe("APIM Test", () => {
+  describe(`Test case: ${APP_NAME} endpoints`, () => {
+    test(`redirects http to https for ${APP_NAME}`, async () => {
+      try {
+        const url = `http://${TEST_URL}/`;
+        const response: AxiosResponse = await axios
+          .request({
+            method: "GET",
+            url: url,
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Accept-Encoding": "gzip",
+            },
+            maxRedirects: 0,
+            validateStatus: false,
+          })
+
+        expect(response.status).toBe(301);
+        expect(response.headers.location).toBe(
+          `https://${TEST_URL}/`
+        );
+      } catch(error) {
+        fail(error.stack);
+      }
+    });
+    test(`app is healthy (${APP_NAME}/health)`, async () => {
+      try {
+        const url = `https://${TEST_URL}/health`;
+        const response: AxiosResponse = await axios
+          .request({
+            method: "GET",
+            url: url,
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Accept-Encoding": "gzip",
+            },
+          })
+
+        expect(response.data.status).toBe("UP");
+      } catch(error) {
+        fail(error.stack);
+      }
+    });
+    test(`expected content loads (${APP_NAME}/)`, async () => {
+      try {
+        const url = `https://${TEST_URL}/`;
+        const response: AxiosResponse = await axios
+          .request({
+            method: "GET",
+            url: url,
+            headers: {
+              "Accept-Encoding": "gzip",
+            },
+          })
+        expect(response.status).toBe(200);
+      } catch(error) {
+        fail(error.stack);
+      }
+    });
+  });
+});

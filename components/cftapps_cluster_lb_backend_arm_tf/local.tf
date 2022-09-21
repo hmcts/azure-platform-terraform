8f0e978da8d667e9c9a7ed3b11408f78d584d40b
ciaranca <ciaranca@users.noreply.github.com>
2022-09-21 20:34:39 +0000

    Apply automatic changes

--- components/cftapps_cluster_lb_backend_arm_tf/local.tf
+++ components/cftapps_cluster_lb_backend_arm_tf/local.tf
@@ -3,6 +3,6 @@ locals {
 
   vnet_rg   = var.env == "prod" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
   vnet_name = var.env == "prod" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
-  
+
   key_vault_resource_group = "cft-platform-${var.subscription}-rg"
 }

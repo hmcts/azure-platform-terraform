a5699d7fc726c8f0543d64e26b9adfb17fb7face
mokainos <72137340+mokainos@users.noreply.github.com>
2022-10-21 11:15:41 +0100

    update

--- components/cftapps_cluster_lb_arm_tf/local.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  vnet_rg   = var.env == "prod" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
-  vnet_name = var.env == "prod" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
-}

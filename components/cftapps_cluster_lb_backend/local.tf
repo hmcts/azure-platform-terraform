1b71dc41597fd875b61b0d5910053a29cfbcb755
mokainos <72137340+mokainos@users.noreply.github.com>
2022-11-14 09:34:00 +0000

    initial

--- components/cftapps_cluster_lb_backend/local.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  env = (var.env == "aat") ? "stg" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
-
-  vnet_rg   = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" || var.env == "demo" ? "cft-${var.env}-network-rg" : "aks-infra-${var.env}-rg"
-  vnet_name = var.env == "sbox" || var.env == "perftest" || var.env == "aat" || var.env == "ithc" || var.env == "demo" ? "cft-${var.env}-vnet" : "core-${var.env}-vnet"
-
-  key_vault_resource_group = "cft-platform-${var.subscription}-rg"
-}

e02e6f5eae0b81d90a753ebe0e8e03876db7253d
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-04-25 15:20:18 +0100

    Switch to master branch of frontdoor module

--- components/global/main.tf
+++ components/global/main.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 module "landing_zone" {
-  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=redirect-feature"
+  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=master"
 
   common_tags                = module.ctags.common_tags
   env                        = var.env

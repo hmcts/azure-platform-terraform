bc29010ae20b303bd9795c7d497ad42aa009bd60
Daniel Wilson <daniel.wilson@kainos.com>
2023-05-23 16:17:39 +0100

    initial changes to handle ssl platform.hmcts.net records

--- components/cftapps_private_dns/main.tf
+++ components/cftapps_private_dns/main.tf
@@ -5,16 +5,44 @@ locals {
 
   gateways = yamldecode(data.local_file.configuration.content).gateways
 
-  a_records = flatten([
-    for gateways, gateway in local.gateways : [
-      for app in gateway.app_configuration : {
-        name   = "${app.product}-${app.component}-${local.dns_zone}",
-        ttl    = 300,
+  # <XXX>.internal
+  internal_records = flatten([
+  for gateways, gateway in local.gateways : [
+    for app in gateway.app_configuration : {
+        name   = "${app.product}-${app.component}-${local.dns_zone}"
+        ttl    = 300
         record = ["${gateway.gateway_configuration.private_ip_address}"]
-      }
-    ]
+      } if !can(app, "ssl_enabled") || app.ssl_enabled == false
+  ]
   ])
 
+  # <XXX>.<ENV>.platform.hmcts.net
+  platform_records = flatten([
+  for gateways, gateway in local.gateways : [
+    for app in gateway.app_configuration : {
+        name   = "${app.product}-${app.component}-${local.dns_zone}"
+        ttl    = 300
+        record = ["${gateway.gateway_configuration.private_ip_address}"]
+      } if can(app, "ssl_enabled") && app.ssl_enabled == true
+  ]
+  ])
+}
+
+variable "dns_records" {
+  type = map(object({
+    a_recordset = string
+    zone_name = string
+  }))
+  default = {
+     internal_dns = {
+      a_recordset = local.internal_records
+      zone_name = "service.core-compute-${local.dns_zone}.internal"
+    }
+    platform_dns = {
+      a_recordset = local.platform_records
+      zone_name = "${local.env}.platform.hmcts.net"
+    }
+  }
 }
 
 data "local_file" "configuration" {
@@ -22,11 +50,12 @@ data "local_file" "configuration" {
 }
 
 module "privatedns" {
+  for_each = var.dns_records
+
   source              = "git::https://github.com/hmcts/azure-private-dns.git//modules/azure-private-dns?ref=master"
-  a_recordsets        = local.a_records
+  a_recordsets        = each.value.a_recordset
   env                 = local.dns_zone
   resource_group_name = "core-infra-intsvc-rg"
-  zone_name           = "service.core-compute-${local.dns_zone}.internal"
+  zone_name           = each.value.zone_name
 
 }
-#

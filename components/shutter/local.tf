dab85f914ebd73cef4e32e7909210c389fc6ca86
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-08-07 12:13:28 +0100

    update

--- components/shutter/local.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  # List of apps to deploy shuttering infra for. 
-  shutter_apps = [for k, v in var.frontends : v if lookup(v, "shutter_app", true)]
-}
--- modules/shutter_page/data.tf
+++
@@ -1,5 +0,0 @@
-data "azurerm_resource_group" "shutter" {
-  name = var.resource_group_name
-}
-
-data "azurerm_subscription" "current" {}

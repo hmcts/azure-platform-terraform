a119a4841ea91b840d068859264157e21ca0128f
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-08-11 09:27:30 +0100

    added downloading root cert changes

---
+++ components/apim-appgw/download_root_certs.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+#grabing the public root CA 
+
+curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
+#grabing the public intermediate CA
+curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
+
+cat root.pem intermediate.pem >merged.pem
+
+echo -n "{\"data\":\"nothing\"}"

695b5b7d0fb929ff3eabb8cb31695ff613d0b975
ridoutp <paul.ridout@cgi.com>
2023-09-28 11:06:22 +0100

    Set app gateway rewrite rules from config
    Changed terraform-module-apim-application-gateway branch name to one that includes both mTLS and rewrite rule changes.

--- components/apim-appgw/main.tf
+++ components/apim-appgw/main.tf
@@ -27,7 +27,7 @@ module "app-gw" {
     azurerm.kv  = azurerm.kv
   }
 
-  source                                       = "git::https://github.com/hmcts/terraform-module-apim-application-gateway.git?ref=general-rewrite-rule-set"
+  source                                       = "git::https://github.com/hmcts/terraform-module-apim-application-gateway.git?ref=multiple-ssl-profile-general-rewrite-rule-set"
   yaml_path                                    = "${path.cwd}/../../environments/${local.env}/apim_appgw_config.yaml"
   env                                          = local.dns_zone
   location                                     = var.location

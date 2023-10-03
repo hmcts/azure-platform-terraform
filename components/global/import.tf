359f836f7545d5890974c937ee6f3e7c4eb4e3ae
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-10-03 12:36:29 +0100

    corrected import

--- components/global/import.tf
+++ components/global/import.tf
@@ -1,14 +1,24 @@
 import {
-  to = module.frontdoor_classic[0].azurerm_cdn_frontdoor_profile.front_door
+  to = module.premium_front_door[0].azurerm_cdn_frontdoor_profile.front_door
   id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.main.name}/providers/Microsoft.Cdn/profiles/${var.project}-${var.env}"
 }
 
 import {
-  to = module.frontdoor_classic[0].azurerm_cdn_frontdoor_endpoint.endpoint
+  to = module.premium_front_door[0].azurerm_cdn_frontdoor_endpoint.endpoint
   id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.main.name}/providers/Microsoft.Cdn/profiles/${var.project}-${var.env}/afdEndpoints/${var.project}-${var.env}"
 }
 
 import {
-  to = module.frontdoor_classic[0].azurerm_cdn_frontdoor_origin_group.defaultBackend
+  to = module.premium_front_door[0].azurerm_cdn_frontdoor_origin_group.defaultBackend
   id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.main.name}/providers/Microsoft.Cdn/profiles/${var.project}-${var.env}/originGroups/defaultBackend"
+}
+
+import {
+  to = module.premium_front_door[0].azurerm_cdn_frontdoor_route.default_routing_rule
+  id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.main.name}/providers/Microsoft.Cdn/profiles/${var.project}-${var.env}/afdEndpoints/${var.project}-${var.env}/routes/defaultRouting"
+}
+
+import {
+  to = module.premium_front_door[0].azurerm_monitor_diagnostic_setting.frontdoor_diagnostics
+  id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.main.name}/providers/Microsoft.Cdn/profiles/${var.project}-${var.env}|fd-log-analytics"
 }

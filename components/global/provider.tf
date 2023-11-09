bb0315ea77396583672ab112a2e71846246f9d38
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-11-09 02:22:44 +0000

    done

--- components/global/provider.tf
+++ components/global/provider.tf
@@ -1,13 +1,7 @@
 terraform {
   required_version = ">= 0.13.0"
 
-  backend "azurerm" {
-    resource_group_name  = "azure-control-prod-rg"
-    storage_account_name = "c8cbc6f36739db5d00b27sa"
-    container_name       = "subscription-tfstate"
-    key                  = "UK South/cft-platform/azure-platform-terraform/prod/global/terraform.tfstate"
-    subscription_id      = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
-  }
+  backend "azurerm" {}
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"

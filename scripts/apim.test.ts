9008ad0742be4a85cde2d2770ba2a7823c2cadbf
Thomas Thornton <t.thornton@kainos.com>
2023-12-11 14:06:11 +0000

    add apim test and depend prod on stg apim (#1910)
    * test apim.tests.ts
    
    * test apim URL
    
    * add actual health check
    
    * update status to be 200 test
    
    * just testing a non-existent /URI
    
    * full pipeline test
    
    * revert condition
    
    ---------
    
    Co-authored-by: Thomas Thornton <>

--- scripts/apim.test.ts
+++ scripts/apim.test.ts
@@ -2,35 +2,26 @@ import { fail } from "assert";
 
 import axios, { AxiosResponse } from "axios";
 
-// APP_NAME will be set to the value of plum or toffee
-const APP_NAME =
-  process.env.APP_NAME
-
-
-// TEST_URL will be set to ${product}.${environment}.platform.hmcts.net
-const TEST_URL =
-  process.env.TEST_URL
+// TEST_URL will be set to ${APIM_NAME}.${environment}.platform.hmcts.net
+const APIM_TEST_URL =
+  process.env.APIM_TEST_URL
 
 describe("Test Name", () => {
-  describe(`Test case`, () => {
-    test(`Success message`, async () => {
+  describe(`Test case ${APIM_TEST_URL}`, () => {
+    test(`apim is healthy (${APIM_TEST_URL}/health/liveness)`, async () => {
       try {
+        const url = `https://${APIM_TEST_URL}/health/liveness`;
+        const response: AxiosResponse = await axios
+          .request({
+            method: "GET",
+            url: url,
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Accept-Encoding": "gzip",
+            },
+          })
 
-        // Placeholder test to check TEST_URL is defined
-        expect(TEST_URL).toBeDefined();
-
-        // Example test: perform a GET request against your applications liveness endpoint
-        // const url = `https://${TEST_URL}/health/liveness`;
-        // const response: AxiosResponse = await axios
-        //   .request({
-        //     method: "GET",
-        //     url: url,
-        //     headers: {
-        //       "Accept-Encoding": "gzip",
-        //     },
-        //   })
-        // Example response: expect the HTTP status code 200
-        // expect(response.status).toBe(200);
+        expect(response.status).toBe(200);
       } catch(error) {
         fail(error.stack);
       }

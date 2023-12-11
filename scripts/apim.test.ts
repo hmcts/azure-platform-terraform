8295755b59d84a54bc7dde7f926be7998ada9405
Thomas Thornton <>
2023-12-11 12:13:05 +0000

    full pipeline test

--- scripts/apim.test.ts
+++ scripts/apim.test.ts
@@ -2,12 +2,7 @@ import { fail } from "assert";
 
 import axios, { AxiosResponse } from "axios";
 
-// APP_NAME will be set to the value of plum or toffee
-const APP_NAME =
-  process.env.APP_NAME
-
-
-// TEST_URL will be set to ${product}.${environment}.platform.hmcts.net
+// TEST_URL will be set to ${APIM_NAME}.${environment}.platform.hmcts.net
 const APIM_TEST_URL =
   process.env.APIM_TEST_URL
 
@@ -15,7 +10,7 @@ describe("Test Name", () => {
   describe(`Test case ${APIM_TEST_URL}`, () => {
     test(`apim is healthy (${APIM_TEST_URL}/health/liveness)`, async () => {
       try {
-        const url = `https://${APIM_TEST_URL}/health/liveness1`;
+        const url = `https://${APIM_TEST_URL}/health/liveness`;
         const response: AxiosResponse = await axios
           .request({
             method: "GET",

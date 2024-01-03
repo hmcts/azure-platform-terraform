d5e7bcc5ed7b725c6d789d96e5e185cdb4ff7b68
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-01-03 15:15:14 +0000

    Update main.tf

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -19,6 +19,7 @@ module "api-mgmt" {
   common_tags                    = module.ctags.common_tags
   route_next_hop_in_ip_address   = local.hub[var.hub].ukSouth.next_hop_ip
   publisher_email                = var.publisher_email
+}
 
 resource "azurerm_api_management_named_value" "environment" {
   name                = "environment"

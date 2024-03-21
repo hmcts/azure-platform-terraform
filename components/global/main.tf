56264083a1c455ec20c644cc6ca4c829f705a40c
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-03-21 15:18:29 +0000

    change branch back to master

--- components/global/main.tf
+++ components/global/main.tf
@@ -9,7 +9,7 @@ moved {
   to   = module.premium_front_door
 }
 module "premium_front_door" {
-  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=DTSPO-16863-front-door-test"
+  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=master"
 
   common_tags                = module.ctags.common_tags
   env                        = var.env

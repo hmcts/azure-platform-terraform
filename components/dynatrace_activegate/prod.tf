6f538a82cb69d5b25ce670a505c0f9faed62ceb2
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-04-09 08:56:20 +0100

    DTSPO-16138 - Remove dynatrace activegates (#2054)

--- components/dynatrace_activegate/prod.tf
+++ components/dynatrace_activegate/prod.tf
@@ -1,15 +1,15 @@
-module "dynatrace_activegate" {
+# module "dynatrace_activegate" {
 
-  providers = {
-    azurerm     = azurerm
-    azurerm.law = azurerm.law_prod
-    azurerm.soc = azurerm.soc
-  }
+#   providers = {
+#     azurerm     = azurerm
+#     azurerm.law = azurerm.law_prod
+#     azurerm.soc = azurerm.soc
+#   }
 
-  source               = "../../modules/dynatrace-activegate"
-  instance_count       = 3
-  network_zone         = "azure.cft"
-  install_splunk_uf    = true
-  enable_log_analytics = true
-  common_tags          = module.ctags.common_tags
-}
+#   source               = "../../modules/dynatrace-activegate"
+#   instance_count       = 3
+#   network_zone         = "azure.cft"
+#   install_splunk_uf    = true
+#   enable_log_analytics = true
+#   common_tags          = module.ctags.common_tags
+# }

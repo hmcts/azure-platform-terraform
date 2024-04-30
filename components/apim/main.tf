247f0b24bb378022529b535f89a4c31c574ad2ce
Joshua Glass <joshua.glass@justice.gov.uk>
2024-04-30 10:25:50 +0100

    testing module

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -6,23 +6,6 @@ module "ctags" {
   expiresAfter = var.expiresAfter
 }
 
-data "azurerm_subnet" "temp_subnet" {
-  name                 = "iaas"
-  resource_group_name  = local.vnet_rg
-  virtual_network_name = local.vnet_name
-}
-
-resource "azurerm_public_ip" "temp_pip" {
-  name                = "temp-pip"
-  resource_group_name = local.vnet_rg
-  location            = var.location
-  allocation_method   = "Static"
-  domain_name_label   = "temp-pip"
-
-  tags = module.ctags.common_tags
-  sku  = "Standard"
-}
-
 module "api-mgmt" {
   source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=DTSPO-17136-apim-upgrade"
   location                       = var.location
@@ -35,9 +18,6 @@ module "api-mgmt" {
   common_tags                    = module.ctags.common_tags
   route_next_hop_in_ip_address   = local.hub[var.hub].ukSouth.next_hop_ip
   publisher_email                = var.publisher_email
-  temp_subnet_id                 = var.trigger_migration == true ? data.azurerm_subnet.temp_subnet.id : null
-  temp_pip_id                    = var.trigger_migration == true ? azurerm_public_ip.temp_pip.id : null
-
 }
 
 resource "azurerm_api_management_named_value" "environment" {

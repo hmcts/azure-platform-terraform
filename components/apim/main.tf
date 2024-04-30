06fa387c99c9397cbfbe75b5cd9fe43dacbadb7f
Joshua Glass <joshua.glass@justice.gov.uk>
2024-04-30 10:14:26 +0100

    testing apim main

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -1,4 +1,3 @@
-
 module "ctags" {
   source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
   environment  = var.env
@@ -7,8 +6,25 @@ module "ctags" {
   expiresAfter = var.expiresAfter
 }
 
+data "azurerm_subnet" "temp_subnet" {
+  name                 = "iaas"
+  resource_group_name  = local.vnet_rg
+  virtual_network_name = local.vnet_name
+}
+
+resource "azurerm_public_ip" "temp_pip" {
+  name                = "temp-pip"
+  resource_group_name = local.vnet_rg
+  location            = var.location
+  allocation_method   = "Static"
+  domain_name_label   = "temp-pip"
+
+  tags = module.ctags.common_tags
+  sku  = "Standard"
+}
+
 module "api-mgmt" {
-  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=DTSPO-17136-apim-upgrade"
   location                       = var.location
   sku_name                       = var.apim_sku_name
   virtual_network_resource_group = local.vnet_rg
@@ -19,6 +35,9 @@ module "api-mgmt" {
   common_tags                    = module.ctags.common_tags
   route_next_hop_in_ip_address   = local.hub[var.hub].ukSouth.next_hop_ip
   publisher_email                = var.publisher_email
+  temp_subnet_id                 = var.trigger_migration == true ? data.azurerm_subnet.temp_subnet.id : null
+  temp_pip_id                    = var.trigger_migration == true ? azurerm_public_ip.temp_pip.id : null
+
 }
 
 resource "azurerm_api_management_named_value" "environment" {

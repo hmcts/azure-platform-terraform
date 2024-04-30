6dd87d3f46c4934871ef12188ed7d76d8c198c68
Joshua Glass <joshua.glass@justice.gov.uk>
2024-04-30 09:09:36 +0100

    testing data block ref

--- components/apim-migrate/main.tf
+++ components/apim-migrate/main.tf
@@ -6,6 +6,11 @@ module "ctags" {
   expiresAfter = var.expiresAfter
 }
 
+data "azurerm_api_management" "apim" {
+  name                = "${var.department}-api-mgmt-${var.env}"
+  resource_group_name = local.vnet_rg
+}
+
 # resource "azurerm_subnet" "temp_subnet" {
 #   name = "temp-migration-subnet"
 #   virtual_network_name = local.vnet_name
@@ -32,16 +37,16 @@ resource "azurerm_public_ip" "temp_pip" {
 
 module "api-mgmt" {
   source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=DTSPO-17136-apim-upgrade"
-  location                       = var.location
-  sku_name                       = var.apim_sku_name
-  virtual_network_resource_group = local.vnet_rg
-  virtual_network_name           = local.vnet_name
-  environment                    = var.env
+  location                       = data.azurerm_api_management.apim.location
+  sku_name                       = data.azurerm_api_management.apim.apim_sku_name
+  virtual_network_resource_group = data.azurerm_api_management.apim.vnet_rg
+  virtual_network_name           = data.azurerm_api_management.apim.vnet_name
+  environment                    = data.azurerm_api_management.apim.env
   virtual_network_type           = "Internal"
-  department                     = var.department
+  department                     = data.azurerm_api_management.apim.department
   common_tags                    = module.ctags.common_tags
   route_next_hop_in_ip_address   = local.hub[var.hub].ukSouth.next_hop_ip
-  publisher_email                = var.publisher_email
+  publisher_email                = data.azurerm_api_management.apim.publisher_email
   temp_subnet_id                 = var.trigger_migration == true ? data.azurerm_subnet.temp_subnet.id : null
   temp_pip_id                    = var.trigger_migration == true ? azurerm_public_ip.temp_pip.id : null
 }

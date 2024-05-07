ba1560983a551af4c3fa6b9e4c8ace8746291503
Joshua Glass <joshua.glass@justice.gov.uk>
2024-05-07 15:03:03 +0100

    added migration env

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -19,6 +19,7 @@ module "api-mgmt" {
   route_next_hop_in_ip_address   = local.hub[var.hub].ukSouth.next_hop_ip
   publisher_email                = var.publisher_email
   trigger_migration              = var.trigger_migration
+  migration_env                  = var.migration_env
 }
 
 resource "azurerm_api_management_named_value" "environment" {

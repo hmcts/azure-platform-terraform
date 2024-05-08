b588a31d57404511b724862dae7469b4eb3f00ca
Joshua Glass <joshua.glass@justice.gov.uk>
2024-05-08 10:07:12 +0100

    final plan

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -7,7 +7,7 @@ module "ctags" {
 }
 
 module "api-mgmt" {
-  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=DTSPO-17136-apim-upgrade"
   location                       = var.location
   sku_name                       = var.apim_sku_name
   virtual_network_resource_group = local.vnet_rg

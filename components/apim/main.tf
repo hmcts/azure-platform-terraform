771bf6a407a0369222f607a82c4b1bb83483f6c2
Joshua Glass <141819030+JoshuaGlassKainos@users.noreply.github.com>
2024-05-21 09:38:50 +0100

    update ref to cft branch in module (#2121)

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -7,7 +7,7 @@ module "ctags" {
 }
 
 module "api-mgmt" {
-  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=apim-cft-upgrade"
   location                       = var.location
   sku_name                       = var.apim_sku_name
   virtual_network_resource_group = local.vnet_rg

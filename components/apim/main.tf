1543b7ae5cf031d91eb92c234bd0f4660835aced
Joshua Glass <141819030+JoshuaGlassKainos@users.noreply.github.com>
2024-05-31 17:17:09 +0100

    revert apim module, apim upgrade complete (#2141)
    * test cleanup
    
    * removed migration vars no longer needed
    
    * remove stg migration vars
    
    * chnage apim ref to main

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -7,7 +7,7 @@ module "ctags" {
 }
 
 module "api-mgmt" {
-  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=apim-cft-upgrade"
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
   location                       = var.location
   sku_name                       = var.apim_sku_name
   virtual_network_resource_group = local.vnet_rg
@@ -18,7 +18,6 @@ module "api-mgmt" {
   common_tags                    = module.ctags.common_tags
   route_next_hop_in_ip_address   = local.hub[var.hub].ukSouth.next_hop_ip
   publisher_email                = var.publisher_email
-  migration_variables            = var.migration_variables
 }
 
 resource "azurerm_api_management_named_value" "environment" {

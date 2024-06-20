08bc2b703f139ec65ee6f27c764590b56ff411d3
Daniel Wilson <daniel.wilson@kainos.com>
2024-06-20 10:44:22 +0100

    tf fmt

--- components/apim-appgw/root_certs.tf
+++ components/apim-appgw/root_certs.tf
@@ -42,23 +42,23 @@ locals {
 
   trusted_client_certificates = {
     sbox = {
-      lets_encrypt         = "${path.module}/merged.pem"
+      lets_encrypt = "${path.module}/merged.pem"
     }
     test = {
-      lets_encrypt         = "${path.module}/merged.pem"
-      civil_sdt_root_ca    = "civil-sdt-root-ca"
-      reform_scan_sscs_ca  = "reform-scan-sscs-ca"
-      dts_bsp_team_ca      = "dts-bsp-team-ca"
-      exela_uat_ca         = "exela-uat-ca"
-      iron_mountain_ca     = "iron-mountain-ca"
+      lets_encrypt        = "${path.module}/merged.pem"
+      civil_sdt_root_ca   = "civil-sdt-root-ca"
+      reform_scan_sscs_ca = "reform-scan-sscs-ca"
+      dts_bsp_team_ca     = "dts-bsp-team-ca"
+      exela_uat_ca        = "exela-uat-ca"
+      iron_mountain_ca    = "iron-mountain-ca"
     }
     prod = {
-      civil_sdt_root_ca    = "civil-sdt-root-ca"
+      civil_sdt_root_ca = "civil-sdt-root-ca"
     }
   }
 
   trusted_client_certificate_data = lookup(local.trusted_client_certificates, local.env, {})
-  cert_names = [for cert_name, cert_value in local.trusted_client_certificate_data : cert_name if cert_value != ""]
+  cert_names                      = [for cert_name, cert_value in local.trusted_client_certificate_data : cert_name if cert_value != ""]
 }
 
 
@@ -70,8 +70,8 @@ data "azurerm_key_vault" "key_vault" {
 }
 
 data "azurerm_key_vault_secret" "secrets" {
-  for_each    = toset(local.cert_names)
-  name        = lookup(local.trusted_client_certificate_data, each.key)
+  for_each     = toset(local.cert_names)
+  name         = lookup(local.trusted_client_certificate_data, each.key)
   key_vault_id = data.azurerm_key_vault.key_vault.id
 
   provider = azurerm.kv

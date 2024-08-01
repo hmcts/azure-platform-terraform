f788b5c5516cb5c6a3acfe046f4cdcce28a87d38
Daniel Wilson <102226203+danielwilsonkainos@users.noreply.github.com>
2024-08-01 15:23:08 +0100

    DTSPO-18453: deploy demo apim appgw (#2217)
    * add apim_appgw yaml config, map mtls certs
    
    * add demo_apim_appgw deployment to pipeline
    
    * add tfvars for demo apim_appgw
    
    * Apply automatic changes
    
    * correct demo yaml config
    
    * add key_vault_subscription demo
    
    * update locals for demo vnet assignment
    
    ---------
    
    Co-authored-by: danielwilsonkainos <danielwilsonkainos@users.noreply.github.com>

--- components/apim-appgw/local.tf
+++ components/apim-appgw/local.tf
@@ -1,8 +1,8 @@
 locals {
   env = var.env == "perftest" ? "test" : var.env
 
-  vnet_rg     = var.env == "ithc" || var.env == "perftest" ? "hmcts-hub-nonprodi" : "hmcts-hub-${var.env}-int"
-  vnet_name   = var.env == "ithc" || var.env == "perftest" ? "hmcts-hub-nonprodi" : "hmcts-hub-${var.env}-int"
+  vnet_rg     = var.env == "ithc" || var.env == "perftest" || var.env == "demo" ? "hmcts-hub-nonprodi" : "hmcts-hub-${var.env}-int"
+  vnet_name   = var.env == "ithc" || var.env == "perftest" || var.env == "demo" ? "hmcts-hub-nonprodi" : "hmcts-hub-${var.env}-int"
   subnet_name = "hub-appgw"
   hub_env     = (var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "perftest") ? "nonprod" : (var.env == "stg") ? "prod" : var.env
 

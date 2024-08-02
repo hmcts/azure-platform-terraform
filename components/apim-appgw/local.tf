e7e1aeb136fdc179fccf4d3da45dfac490d640ee
Daniel Wilson <daniel.wilson@kainos.com>
2024-08-02 12:02:35 +0100

    update hub_env

--- components/apim-appgw/local.tf
+++ components/apim-appgw/local.tf
@@ -4,7 +4,7 @@ locals {
   vnet_rg     = var.env == "ithc" || var.env == "perftest" || var.env == "demo" ? "hmcts-hub-nonprodi" : "hmcts-hub-${var.env}-int"
   vnet_name   = var.env == "ithc" || var.env == "perftest" || var.env == "demo" ? "hmcts-hub-nonprodi" : "hmcts-hub-${var.env}-int"
   subnet_name = "hub-appgw"
-  hub_env     = (var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "perftest") ? "nonprod" : (var.env == "aat") ? "prod" : var.env
+  hub_env     = (var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "perftest") ? "nonprod" : (var.env == "stg") ? "prod" : var.env
 
   key_vault_resource_group = "cft-platform-${var.subscription}-rg"
   key_vault_subscription   = var.key_vault_subscription

c30dd55cafcf91cd21811b96ca91b26eb6cc5e13
Daniel Wilson <daniel.wilson@kainos.com>
2024-08-20 14:15:42 +0100

    swap api-mgmt module to master

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -7,7 +7,7 @@ module "ctags" {
 }
 
 module "api-mgmt" {
-  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=dtspo-18411-apim-log-issue"
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=master"
   location                       = var.location
   sku_name                       = var.apim_sku_name
   virtual_network_resource_group = local.vnet_rg

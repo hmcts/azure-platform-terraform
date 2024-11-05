63e567b1b577acaf9215fdff58cfcfca70daa7aa
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-11-05 15:44:02 +0000

    add enable_waf

--- components/frontendappgateway/main.tf
+++ components/frontendappgateway/main.tf
@@ -34,4 +34,5 @@ module "frontendappgateway" {
   min_capacity                       = var.frontend_agw_min_capacity
   max_capacity                       = var.frontend_agw_max_capacity
   diagnostics_storage_account_id     = data.azurerm_storage_account.diagnostics.id
+  enable_waf                         = var.enable_waf
 }

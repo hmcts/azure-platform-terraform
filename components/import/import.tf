7e4d352eea3c413f9465e1c4dfbd5cdbf9a651f9
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2024-11-13 15:39:19 +0000

    adding import block for error on pipeline

---
+++ components/import/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.api-mgmt.azurerm_api_management_custom_domain.api-management-custom-domain
+  id = "/subscriptions/96c274ce-846d-4e48-89a7-d528432298a7/resourceGroups/cft-aat-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-stg"
+}

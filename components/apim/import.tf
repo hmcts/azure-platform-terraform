1617fe337f767d2c7ab68c6c327679452acda9db
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2024-11-13 16:55:10 +0000

    adding import block for error on pipeline

--- components/apim/import.tf
+++ components/apim/import.tf
@@ -1,4 +1,14 @@
 import {
   to = module.api-mgmt.azurerm_api_management_custom_domain.api-management-custom-domain
-  id = "/subscriptions/96c274ce-846d-4e48-89a7-d528432298a7/resourceGroups/cft-aat-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-stg"
+  id = "/subscriptions/96c274ce-846d-4e48-89a7-d528432298a7/resourceGroups/cft-aat-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-stg/customDomains/cft-api-mgmt.aat.platform.hmcts.net"
+}
+
+import {
+  to = module.api-mgmt.azurerm_api_management_custom_domain.api-management-custom-domain
+  id = "/subscriptions/96c274ce-846d-4e48-89a7-d528432298a7/resourceGroups/cft-aat-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-stg/customDomains/cft-api-mgmt-appgw.aat.platform.hmcts.net"
+}
+
+import {
+  to = module.api-mgmt.azurerm_api_management_custom_domain.api-management-custom-domain
+  id = "/subscriptions/96c274ce-846d-4e48-89a7-d528432298a7/resourceGroups/cft-aat-network-rg/providers/Microsoft.ApiManagement/service/cft-api-mgmt-stg/customDomains/cft-mtls-api-mgmt-appgw.aat.platform.hmcts.net"
 }

2a1fd4701c47d6b74652da130149fa1cc25d5e1e
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-12-11 15:18:40 +0000

    Update main.tf

--- components/frontendappgateway/main.tf
+++ components/frontendappgateway/main.tf
@@ -15,6 +15,28 @@ module "ctags" {
 data "azurerm_subscription" "current" {}
 
 module "frontendappgateway" {
+  source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=master"
+
+  env                                = var.env
+  subscription                       = var.subscription
+  location                           = var.location
+  private_ip_address                 = var.frontend_agw_private_ip_address
+  destinations                       = var.cft_apps_cluster_ips
+  frontends                          = var.frontends
+  common_tags                        = module.ctags.common_tags
+  oms_env                            = var.oms_env
+  project                            = var.project
+  vnet_rg                            = local.vnet_rg
+  vnet_name                          = local.vnet_name
+  log_analytics_workspace_id         = module.logworkspace.workspace_id
+  enable_multiple_availability_zones = true
+  resource_prefix                    = "cft"
+  min_capacity                       = var.frontend_agw_min_capacity
+  max_capacity                       = var.frontend_agw_max_capacity
+  diagnostics_storage_account_id     = data.azurerm_storage_account.diagnostics.id
+}
+
+module "frontendappgateway" {
   source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=WAF_Setup2"
 
   env                                = var.env
@@ -34,4 +56,5 @@ module "frontendappgateway" {
   min_capacity                       = var.frontend_agw_min_capacity
   max_capacity                       = var.frontend_agw_max_capacity
   diagnostics_storage_account_id     = data.azurerm_storage_account.diagnostics.id
+  enable_waf                         = var.enable_waf
 }

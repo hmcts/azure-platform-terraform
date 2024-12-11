211836aaf54740c8a2e361252a103b3d095f9baa
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-12-11 14:34:48 +0000

    Update main.tf

--- components/frontendappgateway/main.tf
+++ components/frontendappgateway/main.tf
@@ -15,7 +15,7 @@ module "ctags" {
 data "azurerm_subscription" "current" {}
 
 module "frontendappgateway" {
-  source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=master"
+  source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=WAF_Setup2"
 
   env                                = var.env
   subscription                       = var.subscription

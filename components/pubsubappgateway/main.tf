8ba6db9a729bb81b9b25c08737c2b8da54e1281c
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2025-01-16 14:55:30 +0000

    fixing typo

--- components/pubsubappgateway/main.tf
+++ components/pubsubappgateway/main.tf
@@ -36,7 +36,7 @@ module "pubsubappgateway" {
   max_capacity                       = var.frontend_agw_max_capacity
   diagnostics_storage_account_id     = data.azurerm_storage_account.diagnostics.id
   enable_waf                         = true
-  app_gateway_name                   = "cft-fe-${format("%02d", )}-${var.env}-agw-pubsub"
+  app_gateway_name                   = "cft-fe-${var.env}-agw-pubsub"
   pip_name                           = "cft-pubsub-fe-appgw-${var.env}-pip"
   pubsub_subnet                      = true
   waf_policy_name                    = "pubsub-waf-policy"

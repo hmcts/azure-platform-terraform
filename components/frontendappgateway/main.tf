ddb2467bfc3a1186c51755fa3de09d95a4d1bd93
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2025-02-18 10:35:49 +0000

    testing changes on frontend gateways

--- components/frontendappgateway/main.tf
+++ components/frontendappgateway/main.tf
@@ -15,7 +15,7 @@ module "ctags" {
 data "azurerm_subscription" "current" {}
 
 module "frontendappgateway" {
-  source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=master"
+  source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=DTSPO-23915-updating-pubsub-gateway"
 
   env                                = var.env
   subscription                       = var.subscription

2d1bf686c70ce6ac94e31a1f67a390b2372b2806
jordankainos <j.brown@kainos.com>
2025-08-13 11:51:11 +0100

    Update main.tf

--- components/apim-appgw/main.tf
+++ components/apim-appgw/main.tf
@@ -49,7 +49,7 @@ module "app-gw" {
   max_capacity                                 = var.apim_appgw_max_capacity
 
   # Control the rollout of the TLS 1.0/1.1 deprecation, the ternary should be removed once the rollout is complete
-  ssl_policy = (local.env != "stg" && local.env != "prod") ? var.ssl_policy : local.current_ssl_policy
+  ssl_policy = (var.env != "stg" && var.env != "prod") ? var.ssl_policy : local.current_ssl_policy
 
   trusted_client_certificate_data = merge(
     {

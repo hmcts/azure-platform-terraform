2173ff5b0f31a2c8d9ffd5d9fc7e067ababf51d8
jordankainos <j.brown@kainos.com>
2025-08-13 11:31:39 +0100

    Update main.tf

--- components/frontendappgateway/main.tf
+++ components/frontendappgateway/main.tf
@@ -45,5 +45,5 @@ module "frontendappgateway" {
   ssl_certificate_name               = var.ssl_certificate
 
   # Control the rollout of the TLS 1.0/1.1 deprecation, the ternary should be removed once the rollout is complete
-  ssl_policy = var.env != "prod" || var.env != "stg" ? local.current_ssl_policy : var.ssl_policy
+  ssl_policy = (local.env != "stg" && local.env != "prod") ? var.ssl_policy : local.current_ssl_policy
 }

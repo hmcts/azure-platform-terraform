ab99e3b4012141ecf4aede3713bc12b7af1d0987
Asaleem-hmcts <Aamir.Saleem@HMCTS.NET>
2025-11-18 15:18:31 +0000

    udpate feature/su

--- components/shutter_static_webapp/static_webapp.tf
+++ components/shutter_static_webapp/static_webapp.tf
@@ -7,7 +7,7 @@ module "static_webapp" {
     azurerm.dnszone = azurerm.dnszone
   }
 
-  source              = "git::https://github.com/hmcts/terraform-module-shutter-static-webapp.git?ref=master"
+  source              = "git::https://github.com/hmcts/terraform-module-shutter-static-webapp.git?ref=feature/su"
   shutter_apps        = local.shutter_apps
   tags                = module.ctags.common_tags
   resource_group_name = azurerm_resource_group.rg.name

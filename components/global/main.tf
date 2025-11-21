5ee4f41c722ad83dbbf4ef03ddeff850a240a753
Kremena Nenkova <kremena.nenkova@hmcts.net>
2025-11-21 16:52:05 +0000

    Define rule_sets for frontdoor

--- components/global/main.tf
+++ components/global/main.tf
@@ -24,6 +24,7 @@ module "premium_front_door" {
   key_vault_resource_group   = data.azurerm_resource_group.key_vault.name
   log_analytics_workspace_id = module.log_analytics_workspace.workspace_id
   add_access_policy          = var.add_access_policy
+  rule_sets                  = var.rule_sets
 
   diagnostics_storage_account_id    = azurerm_storage_account.diagnostics.id
   send_access_logs_to_log_analytics = false

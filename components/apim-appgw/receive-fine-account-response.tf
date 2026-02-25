88a275dabd505b5be1c35e11b6f826dc4ab0bbf6
erolkurelihmcts <134499553+erolkurelihmcts@users.noreply.github.com>
2026-02-25 23:23:58 +0000

    Apply automatic changes

--- components/apim-appgw/receive-fine-account-response.tf
+++ components/apim-appgw/receive-fine-account-response.tf
@@ -42,7 +42,7 @@ resource "azurerm_api_management_product" "gob" {
   published             = true
   subscription_required = true
   approval_required     = false
-  tags = var.common_tags
+  tags                  = var.common_tags
   # Represents the gob product
 }
 
@@ -51,6 +51,6 @@ resource "azurerm_api_management_product_api" "gob-receive-fine-account-response
   product_id          = azurerm_api_management_product.gob.product_id
   api_management_name = var.api_management_name
   resource_group_name = var.resource_group_name
-  tags = var.common_tags
+  tags                = var.common_tags
   # Adds receive-fine-account-response API to gob product
 }
